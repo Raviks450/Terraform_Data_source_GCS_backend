6e0d80ca7506d0dba110ebc463673adfe3af8110
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-03-31 08:16:37 +0100

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "gcs" {
+    bucket   = "terraform-state-files-2029"
+    prefix = "terraform/state/network"
+  }
+}
+
+/*#GCS Backend
+terraform {
+  backend "gcs"{
+    bucket   = "terraform-state-files-2029"
+    prefix = "terraform/state/gcp-vm.tfstate"
+  }
+}*/
+
+/*#Local Backend
+terraform {
+  backend "gcs" {
+    path = "network.tfstate"
+  }
+}*/
+
